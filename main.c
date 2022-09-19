05187fcbe30307d49d304a03ed9626be6a7aaa76
Dharmishtha-kanani <113577558+Dharmishtha-kanani@users.noreply.github.com>
2022-09-19 22:04:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    printf("Name= Dharmishtha kanani");
+    printf("\nBirthdate= 31 octomber 1992");
+    printf("\nAge=29");
+    printf("\nAddress=B-403 bhagyalaxmi appartment,near halar house,jamnagar");
+    
+}
