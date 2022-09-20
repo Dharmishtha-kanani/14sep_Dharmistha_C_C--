e79eb93d5723adb4b27ac1a6cfc5c655f92fd271
Dharmishtha-kanani <113577558+Dharmishtha-kanani@users.noreply.github.com>
2022-09-20 12:03:01 +0530

    Add files via upload

---
+++ assignment/main (12).c
@@ -0,0 +1,12 @@
+/******************************************************************************
+
+
+*******************************************************************************/
+#include<stdio.h>
+void main()
+{
+    printf("Name= Dharmishtha kanani");
+    printf("\nBirthdate=31 october 1992 ");
+    printf("\nAge=29");
+    printf("\nAddress=B-403 bhagyalaxmi app.,near halar house,jamnagar");
+}
