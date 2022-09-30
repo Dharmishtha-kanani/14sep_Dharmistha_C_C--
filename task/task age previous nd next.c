92e4f69b46ac9e08198ce3f90f6b0eb1383cd3e1
Dharmishtha-kanani <113577558+Dharmishtha-kanani@users.noreply.github.com>
2022-09-30 16:40:02 +0530

    Add files via upload

---
+++ task/task age previous nd next.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int currentage;
+    printf("my current age is :");
+    scanf("%d",&currentage);
+    currentage++;
+    printf("my next year age is :%d",currentage);
+    currentage--;
+    currentage--;
+    printf("\nmy previous year age is :%d",currentage);
+}
