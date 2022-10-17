44160433970a27d05f9f9d7f3071d4b53ad60d5e
Dharmishtha-kanani <113577558+Dharmishtha-kanani@users.noreply.github.com>
2022-10-17 14:53:47 +0530

    Add files via upload

---
+++ task/nested loop .c.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int i,j,number;
+    
+    for(i=0; i<=5; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf(" %d",number);
+            
+            number++;
+        }    
+        
+        printf("\n");
+    }
+}
+   
