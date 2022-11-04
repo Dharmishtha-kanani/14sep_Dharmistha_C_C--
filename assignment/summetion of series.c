d65561de6413a28d60cc6c5569f9d450bc8eeb17
Dharmishtha-kanani <113577558+Dharmishtha-kanani@users.noreply.github.com>
2022-11-04 09:54:46 +0530

    Add files via upload

---
+++ assignment/summetion of series.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int i,n,sum=0;
+ printf("enter the series:");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ 
+     sum+=i;
+     {
+     printf("sum of series is%d:",sum);
+     }
+ 
+ return 0;
+ 
+}
